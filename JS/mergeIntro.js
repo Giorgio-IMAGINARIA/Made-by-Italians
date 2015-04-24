9ca08e00e22e5b531421f0ce37a628db74ca7446
Giorgio Mazzei <giorgio.mazzei@imaginaria.london>
2015-04-24 23:49:41 +0100

    A website for an Italian startup in Denmark
    I coded this website with HTML, CSS and JavaScript (a slideshow and the
    browser detection)

---
+++ JS/mergeIntro.js
@@ -0,0 +1,23 @@
+﻿window.onload = function () {
+
+
+    playlist = ["../videos/StampingLogo-mobile", "../videos/StampingLogo-tablet", "../videos/StampingLogo-HD"];
+    video = document.getElementById("video");
+    chooseResolution()
+    video.src = playlist[position] + "." + getFormatExtension();
+    video.type = "video/" + getFormatExtension();
+    video.load();
+    video.play();
+
+
+
+
+
+    if (isAndroidBrowser) {
+        // It's Android's native browser (and not Chrome), so do something
+        //alert("Stock Android");
+        changeCSS();
+    }
+
+
+}
